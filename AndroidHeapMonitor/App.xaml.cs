4bd0beb17944bc34e13e49be4641bacd551d50b5
Andreas Willich <andreas.willich@techtalk.at>
2014-10-31 22:01:59 +0100

    add MVVM

--- AndroidHeapMonitor/App.xaml.cs
+++ AndroidHeapMonitor/App.xaml.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using AndroidHeapMonitor.View;
+using AndroidHeapMonitor.ViewModel;
 
 namespace AndroidHeapMonitor
 {
@@ -13,5 +15,14 @@ namespace AndroidHeapMonitor
     /// </summary>
     public partial class App : Application
     {
+        private void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            var mainViewModel = new MainViewModel();
+
+            mainWindow.DataContext = mainViewModel;
+
+            mainWindow.Show();
+        }
     }
 }

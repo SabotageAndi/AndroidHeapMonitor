f32ff74d365c6382271f56dfa95cff6fd048958e
Andreas Willich <andreas.willich@techtalk.at>
2014-10-31 23:38:04 +0100

    Add DumpsysMeminfo

---
+++ AndroidHeapMonitor/Logic/DumpsysMeminfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Managed.Adb;
+
+namespace AndroidHeapMonitor.Logic
+{
+    class DumpsysMeminfo
+    {
+        private readonly Device _device;
+
+        public DumpsysMeminfo(Device device)
+        {
+            _device = device;
+        }
+
+        public string GetInfo(string packageName)
+        {
+            var commandResultReceiver = new CommandResultReceiver();
+            _device.ExecuteShellCommand(string.Format("dumpsys meminfo {0}", packageName), commandResultReceiver );
+
+            return commandResultReceiver.Result;
+        }
+    }
+}
--- AndroidHeapMonitor/ViewModel/MainViewModel.cs
+++ AndroidHeapMonitor/ViewModel/MainViewModel.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Input;
+using AndroidHeapMonitor.Logic;
 using GalaSoft.MvvmLight.Command;
 using Managed.Adb;
 
@@ -12,18 +13,38 @@ namespace AndroidHeapMonitor.ViewModel
 {
     public class MainViewModel : ViewModel
     {
-        private readonly AndroidDebugBridge _bridge;
+        private AndroidDebugBridge _bridge;
         private string _title;
         private Device _device;
+        private string _packageName;
+        private string _output;
+
+        private Timer _refreshTimer;
+        private DumpsysMeminfo _dumpsysMeminfo;
 
         public MainViewModel(AndroidDebugBridge bridge)
         {
             _bridge = bridge;
-            CloseCommand = new RelayCommand(() =>
-            {
-                _device = null;
-                Application.Current.Shutdown();
-            });
+            CloseCommand = new RelayCommand(OnClose);
+            StartCommand = new RelayCommand(OnStart);
+            StopCommand = new RelayCommand(OnStop);
+        }
+
+        private void OnStop()
+        {
+            _refreshTimer.Stop();
+        }
+
+        private void OnStart()
+        {
+            _refreshTimer.Start();
+        }
+
+        private void OnClose()
+        {
+            _device = null;
+            _bridge = null;
+            Application.Current.Shutdown();
         }
 
         public void Init()
@@ -40,10 +61,23 @@ namespace AndroidHeapMonitor.ViewModel
                 Title = "Not connected";
             }
 
+            _refreshTimer = new Timer(2500);
+            _refreshTimer.AutoReset = true;
+            _refreshTimer.Elapsed += _refreshTimer_Elapsed;
+            _dumpsysMeminfo = new DumpsysMeminfo(_device);
+        }
+
+        void _refreshTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var output = _dumpsysMeminfo.GetInfo(PackageName);
+
+            Output = output;
         }
 
 
         public ICommand CloseCommand { get; private set; }
+        public ICommand StartCommand { get; private set; }
+        public ICommand StopCommand { get; private set; }
 
         public string Title
         {
@@ -55,5 +89,28 @@ namespace AndroidHeapMonitor.ViewModel
                 OnPropertyChanged();
             }
         }
+
+        public string PackageName
+        {
+            get { return _packageName; }
+            set
+            {
+                if (value == _packageName) return;
+                _packageName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Output
+        {
+            get { return _output; }
+            set
+            {
+                if (value == _output) return;
+                _output = value;
+                OnPropertyChanged();
+            }
+        }
+
     }
 }

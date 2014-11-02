1128b1115b8fd64f183c677e0797e714e8284161
Andreas Willich <andreas.willich@techtalk.at>
2014-11-02 21:37:10 +0100

    Properties fixes

--- AndroidMemoryMonitor/Properties/AssemblyInfo.cs
+++ AndroidMemoryMonitor/Properties/AssemblyInfo.cs
@@ -19,11 +19,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AndroidHeapMonitor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("AndroidMemoryMonitor")]
+[assembly: AssemblyDescription("Memory Monitor for Android applications")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AndroidHeapMonitor")]
+[assembly: AssemblyProduct("AndroidMemoryMonitor")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
